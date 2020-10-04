5ab5434d7fea1a24057e00cf3e48345378ca6cd6
bolvis <dawid.stasiak17@gmail.com>
2020-10-04 14:36:19 +0200

    Python

---
+++ kwadratowa.py
@@ -0,0 +1,26 @@
+import math
+
+a = input("set a ->")
+b = input("set b ->")
+c = input("set c ->")
+
+def kwadratowa(a, b, c):
+    x1 = 0.0
+    x2 = 0.0
+    if a == 0:
+        return [null,null,null],False
+    delta = pow(b,2) - (4*a*c)
+    if delta >= 0:
+        x1 = -1 * b - math.sqrt(delta)
+        x2 = -1 * b + math.sqrt(delta)
+        possitiveDelta = True
+    else:
+        possitiveDelta = False
+    return [delta,x1,x2],possitiveDelta
+
+r,possitiveDelta = kwadratowa(float(a),float(b),float(c))
+
+if possitiveDelta == True:
+    print("x1 = ",r[1])
+    print("x2 = ",r[2])
+print("Δ = ",r[0])
