ef8e6c6d2618f2810d36a389780967ff8afcdb75
Dawid Stasiak <dawid.stasiak@g2a.com>
2023-03-25 15:34:50 +0100

    go reformat

--- go/kwadratowa.go
+++ go/kwadratowa.go
@@ -7,9 +7,9 @@ import (
 )
 
 func main() {
-	a := getFloat()
-	b := getFloat()
-	c := getFloat()
+	a := getFloat(false, "a")
+	b := getFloat(true, "b")
+	c := getFloat(true, "c")
 
 	positiveDelta, x1, x2, delta := kwadratowa(a, b, c)
 	if positiveDelta {
@@ -22,11 +22,10 @@ func main() {
 func kwadratowa(a, b, c float64) (bool, float64, float64, float64) {
 	var x1 float64
 	var x2 float64
-	
+
 	delta := math.Pow(b, 2) - (4 * a * c)
 
 	positiveDelta := delta >= 0
-
 	if positiveDelta {
 		x1 = ((-1 * b) - math.Sqrt(delta)) / (2 * a)
 		x2 = ((-1 * b) + math.Sqrt(delta)) / (2 * a)
@@ -35,16 +34,25 @@ func kwadratowa(a, b, c float64) (bool, float64, float64, float64) {
 	return positiveDelta, x1, x2, delta
 }
 
-func getFloat() float64 {
-set:
-	fmt.Print("Set a ->")
+func getFloat(canBeZero bool, argumentName string) float64 {
+	fmt.Printf("Set %s ->", argumentName)
 	var x string
-	fmt.Scanln(&x)
-	num, err := strconv.ParseFloat(x, 64)
-	if err != nil || num == 0 {
-		fmt.Println("Something's wrong, try again")
-		goto set
+	_, err := fmt.Scanln(&x)
+	if err != nil {
+		fmt.Println("Input reading error")
+		return getFloat(canBeZero, argumentName)
 	}
-	fmt.Printf("You've set number to %f\n", num)
+
+	num, err := strconv.ParseFloat(x, 64)
+	if err != nil {
+		fmt.Println("Cannot parse input to floating point number")
+		return getFloat(canBeZero, argumentName)
+	}
+	if !canBeZero && num == 0 {
+		fmt.Println("Passed number is 0 which is illegal argument")
+		return getFloat(canBeZero, argumentName)
+	}
+
+	fmt.Printf("You've set %s to %f\n", argumentName, num)
 	return num
 }
